78aa2fa2e394f2123cf9fc2190139f6f62d2c29d
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-09-01 15:42:04 +0200

    starting project

---
+++ lib/widget/biometric.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Biometric extends StatefulWidget {
+  const Biometric({ Key? key }) : super(key: key);
+
+  @override
+  _BiometricState createState() => _BiometricState();
+}
+
+class _BiometricState extends State<Biometric> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
