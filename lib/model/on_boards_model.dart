98a07747646db2b63ee2c4fb49698c581a675a28
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-09-07 10:34:39 +0200

    create board model

---
+++ lib/model/on_boards_model.dart
@@ -0,0 +1,11 @@
+class Onboards {
+  final String text;
+
+  Onboards({required this.text});
+}
+
+List<Onboards> boardsData = [
+  Onboards(text: 'page 1'),
+  Onboards(text: 'page 2'),
+  Onboards(text: 'page 3')
+];
