47c71b64580e287bec69842b4e1ac7196754439f
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-09-08 16:31:07 +0200

    create backend

---
+++ auth/backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// Szerver indítása
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
