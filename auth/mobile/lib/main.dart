2de3730db918f6c7f699aeaf3310d26bef3c1002
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-09-11 17:37:34 +0200

    adding dark mode with provider, also monitors system mod

--- auth/mobile/lib/main.dart
+++ auth/mobile/lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mobile_app/pages/login_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile_app/provider/theme_provider.dart';
 
 void main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
-  
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', home: LoginPage());
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeModeProvider);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: themeMode,
+      home:LoginPage(),
+    );
   }
 }
--- auth/mobile/lib/pages/home.dart
+++ auth/mobile/lib/pages/home.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile_app/provider/theme_provider.dart';
 
-class Home extends StatelessWidget {
+class Home extends ConsumerWidget {
   final String username;
 
   const Home({Key? key, required this.username}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // A MediaQuery segítségével figyeljük az eszköz rendszer témáját
+    final isSystemDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+
+    // Figyeljük, hogy mi van a themeModeProvider-ben
+    final themeMode = ref.watch(themeModeProvider);
+
+    // A dark mode állapot eldöntése a felhasználó és rendszer alapján
+    final isDarkMode = themeMode == ThemeMode.dark ||
+        (themeMode == ThemeMode.system && isSystemDarkMode);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Welcome, $username!'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              isDarkMode ? Icons.dark_mode : Icons.light_mode,
+            ),
+            onPressed: () {
+              ref.read(themeModeProvider.notifier).state =
+                  isDarkMode ? ThemeMode.light : ThemeMode.dark;
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Text(
