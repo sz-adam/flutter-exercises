651c0e48b609b43d3d76a34058f862d7156e482a
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-09-21 16:19:34 +0200

    icon scale animation

--- animation/lib/page/grid_animation_page.dart
+++ animation/lib/page/grid_animation_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class GridAnimationPage extends StatefulWidget {
   const GridAnimationPage({Key? key}) : super(key: key);
 
@@ -19,26 +18,29 @@ class _GridAnimationPageState extends State<GridAnimationPage>
     Icons.camera_alt,
     Icons.home,
     Icons.work,
-  ]; 
+  ];
 
   List<AnimationController> _controllers = [];
   List<Animation<Offset>> _offsetAnimations = [];
+  List<AnimationController> _iconControllers = [];
+  List<Animation<double>> _iconScaleAnimations = [];
 
   @override
   void initState() {
     super.initState();
-    
-    // animácio vezérlő minden rácselemhez
+
+    // Animáció vezérlők minden grid elemhez
     for (int i = 0; i < iconDataList.length; i++) {
+      // SlideTransition animáció
       final controller = AnimationController(
         duration: Duration(milliseconds: 500 + (i * 200)),
         vsync: this,
       );
       _controllers.add(controller);
 
-      // Különbözö irány bal oldal páros index jobb oldalt a páratlan index
+      // Bal oldalra páros, jobb oldalra páratlan index
       final offsetAnimation = Tween<Offset>(
-        begin: i % 2 == 0 ? Offset(-1.5, 0) : Offset(1.5, 0), 
+        begin: i % 2 == 0 ? Offset(-1.5, 0) : Offset(1.5, 0),
         end: Offset(0, 0),
       ).animate(
         CurvedAnimation(
@@ -48,14 +50,35 @@ class _GridAnimationPageState extends State<GridAnimationPage>
       );
       _offsetAnimations.add(offsetAnimation);
 
+      // Indítjuk a slide animációt
       controller.forward();
+
+      // Ikon animációhoz külön AnimationController
+      final iconController = AnimationController(
+        duration: const Duration(seconds: 2),
+        vsync: this,
+      )..forward();
+      _iconControllers.add(iconController);
+
+      // Scale animáció (méret animálása)
+      final scaleAnimation = Tween<double>(begin: 0.1, end: 1.2).animate(
+        CurvedAnimation(
+          parent: iconController,
+          curve: Curves.easeInOut,
+        ),
+      );
+      _iconScaleAnimations.add(scaleAnimation);
     }
   }
 
   @override
   void dispose() {
+    // Megszabadulunk minden AnimationController-től
     for (var controller in _controllers) {
       controller.dispose();
+    }
+    for (var iconController in _iconControllers) {
+      iconController.dispose();
     }
     super.dispose();
   }
@@ -68,8 +91,8 @@ class _GridAnimationPageState extends State<GridAnimationPage>
         padding: const EdgeInsets.all(8.0),
         child: GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, 
-            childAspectRatio: 1, 
+            crossAxisCount: 2,
+            childAspectRatio: 1,
           ),
           itemCount: iconDataList.length,
           itemBuilder: (context, index) {
@@ -78,10 +101,14 @@ class _GridAnimationPageState extends State<GridAnimationPage>
               child: Card(
                 color: Colors.blueAccent,
                 child: Center(
-                  child: Icon(
-                    iconDataList[index],
-                    size: 50,
-                    color: Colors.white,
+                  child: ScaleTransition(
+                    // Ikon méretének animálása
+                    scale: _iconScaleAnimations[index],
+                    child: Icon(
+                      iconDataList[index],
+                      size: 50,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
