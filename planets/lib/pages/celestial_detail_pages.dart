db5b4b834e5e341903fd5a93e14360b9d85e86a7
Szabó Ádám <szaboadam21@gmail.com>
2024-11-18 19:38:58 +0100

    create card animation

--- planets/lib/pages/celestial_detail_pages.dart
+++ planets/lib/pages/celestial_detail_pages.dart
@@ -2,10 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:planets/components/details_card.dart';
 import 'package:planets/model/planets_model.dart';
 
-class CelestialDetailPages extends StatelessWidget {
+class CelestialDetailPages extends StatefulWidget {
   final CelestialBodyData data;
 
   const CelestialDetailPages({super.key, required this.data});
+
+  @override
+  _CelestialDetailPagesState createState() => _CelestialDetailPagesState();
+}
+
+class _CelestialDetailPagesState extends State<CelestialDetailPages>
+    with SingleTickerProviderStateMixin {
+  final List<bool> _isVisible = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _isVisible.addAll(List.generate(5, (index) => false));
+    _animateCards();
+  }
+
+  Future<void> _animateCards() async {
+    for (int i = 0; i < _isVisible.length; i++) {
+      await Future.delayed(const Duration(milliseconds: 500));
+      if (mounted) {
+        setState(() {
+          _isVisible[i] = true;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +39,11 @@ class CelestialDetailPages extends StatelessWidget {
     final screenHeight = MediaQuery.of(context).size.height;
 
     final details = [
-      {'icon': Icons.circle, 'value': data.diameter},
-      {'icon': Icons.line_weight, 'value': data.mass},
-      {'icon': Icons.arrow_downward, 'value': data.gravity},
-      {'icon': Icons.thermostat, 'value': data.temperature},
-      {'icon': Icons.timelapse, 'value': data.orbitalPeriod},
+      {'icon': Icons.circle, 'value': widget.data.diameter},
+      {'icon': Icons.line_weight, 'value': widget.data.mass},
+      {'icon': Icons.arrow_downward, 'value': widget.data.gravity},
+      {'icon': Icons.thermostat, 'value': widget.data.temperature},
+      {'icon': Icons.timelapse, 'value': widget.data.orbitalPeriod},
     ];
 
     return SafeArea(
@@ -25,7 +51,7 @@ class CelestialDetailPages extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.black,
           title: Text(
-            data.name,
+            widget.data.name,
             style: const TextStyle(color: Colors.white),
           ),
           iconTheme: const IconThemeData(
@@ -47,9 +73,9 @@ class CelestialDetailPages extends StatelessWidget {
                     right: screenWidth * 0.05,
                     top: -screenHeight * 0.1,
                     child: Hero(
-                      tag: data.name,
+                      tag: widget.data.name,
                       child: Image.asset(
-                        data.imageUrl,
+                        widget.data.imageUrl,
                         width: 600,
                         height: 500,
                         fit: BoxFit.contain,
@@ -58,10 +84,19 @@ class CelestialDetailPages extends StatelessWidget {
                   ),
                 ],
               ),
-              ...details.map((detail) => DetailCard(
-                icon: detail['icon'] as IconData,  // Passing the icon
-                value: detail['value']! as String,
-              )),
+              ...details.asMap().entries.map((entry) {
+                final index = entry.key;
+                final detail = entry.value;
+                return AnimatedOpacity(
+                  duration: const Duration(milliseconds: 1000),
+                  opacity: _isVisible[index] ? 1.0 : 0.0,
+                  curve: Curves.easeInOut,
+                  child: DetailCard(
+                    icon: detail['icon'] as IconData,
+                    value: detail['value']! as String,
+                  ),
+                );
+              }),
             ],
           ),
         ),
