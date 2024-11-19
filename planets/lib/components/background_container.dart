76af210b4fd3268c258bdc1ccbdfdfc6f45980b6
Szabó Ádám <szaboadam21@gmail.com>
2024-11-19 18:08:34 +0100

    background color setting

---
+++ planets/lib/components/background_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class BackgroundContainer extends StatelessWidget {
+  final Widget child;
+
+  const BackgroundContainer({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: [0.3, 0.9],
+          colors: [
+            Colors.black,
+            Colors.blue,
+          ],
+        ),
+      ),
+      child: child,
+    );
+  }
+}
--- planets/lib/pages/celestial_detail_pages.dart
+++ planets/lib/pages/celestial_detail_pages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:planets/components/background_container.dart';
 import 'package:planets/components/details_card.dart';
 import 'package:planets/model/planets_model.dart';
 
@@ -59,45 +60,47 @@ class _CelestialDetailPagesState extends State<CelestialDetailPages>
           ),
         ),
         backgroundColor: Colors.black,
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    height: screenHeight * 0.5,
-                    alignment: Alignment.center,
-                  ),
-                  Positioned(
-                    right: screenWidth * 0.05,
-                    top: -screenHeight * 0.1,
-                    child: Hero(
-                      tag: widget.data.name,
-                      child: Image.asset(
-                        widget.data.imageUrl,
-                        width: 600,
-                        height: 500,
-                        fit: BoxFit.contain,
+        body: BackgroundContainer(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      height: screenHeight * 0.5,
+                      alignment: Alignment.center,
+                    ),
+                    Positioned(
+                      right: screenWidth * 0.05,
+                      top: -screenHeight * 0.1,
+                      child: Hero(
+                        tag: widget.data.name,
+                        child: Image.asset(
+                          widget.data.imageUrl,
+                          width: 600,
+                          height: 500,
+                          fit: BoxFit.contain,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              ...details.asMap().entries.map((entry) {
-                final index = entry.key;
-                final detail = entry.value;
-                return AnimatedOpacity(
-                  duration: const Duration(milliseconds: 1000),
-                  opacity: _isVisible[index] ? 1.0 : 0.0,
-                  curve: Curves.easeInOut,
-                  child: DetailCard(
-                    icon: detail['icon'] as IconData,
-                    value: detail['value']! as String,
-                  ),
-                );
-              }),
-            ],
+                  ],
+                ),
+                ...details.asMap().entries.map((entry) {
+                  final index = entry.key;
+                  final detail = entry.value;
+                  return AnimatedOpacity(
+                    duration: const Duration(milliseconds: 1000),
+                    opacity: _isVisible[index] ? 1.0 : 0.0,
+                    curve: Curves.easeInOut,
+                    child: DetailCard(
+                      icon: detail['icon'] as IconData,
+                      value: detail['value']! as String,
+                    ),
+                  );
+                }),
+              ],
+            ),
           ),
         ),
       ),
